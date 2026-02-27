01a25fcae4aa6b134bd46f840ff408a8a4d4c626
LQCpaka <boyrovip1539@gmail.com>
2026-02-27 21:46:50 +0700

    cors

--- src/app.rs
+++ src/app.rs
@@ -1,10 +1,12 @@
 use crate::{app_state::AppState, modules};
-use axum::{
-    Router,
-    http::{HeaderName, HeaderValue, Request},
+use axum::Router;
+use axum::http::{
+    HeaderName, HeaderValue, Method, Request,
+    header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE},
 };
 use tower::ServiceBuilder;
 use tower_http::{
+    cors::CorsLayer,
     request_id::{MakeRequestId, PropagateRequestIdLayer, RequestId, SetRequestIdLayer},
     trace::{DefaultOnResponse, TraceLayer},
 };
@@ -24,16 +26,21 @@ impl MakeRequestId for MakeRequestUuid {
 }
 
 pub fn create_router(state: AppState) -> Router {
+    let cors = CorsLayer::new()
+        .allow_origin("http://localhost:3000".parse::<HeaderValue>().unwrap())
+        .allow_methods([
+            Method::GET,
+            Method::POST,
+            Method::PUT,
+            Method::DELETE,
+            Method::PATCH,
+        ])
+        .allow_headers([AUTHORIZATION, ACCEPT, CONTENT_TYPE]);
     Router::new()
         .nest("/api/v1", api_v1())
         .with_state(state)
         .layer(
             ServiceBuilder::new()
-                .layer(SetRequestIdLayer::new(
-                    X_REQUEST_ID.clone(),
-                    MakeRequestUuid,
-                ))
-                .layer(PropagateRequestIdLayer::new(X_REQUEST_ID.clone()))
                 .layer(
                     TraceLayer::new_for_http()
                         .make_span_with(|request: &Request<_>| {
@@ -50,7 +57,13 @@ pub fn create_router(state: AppState) -> Router {
                             )
                         })
                         .on_response(DefaultOnResponse::new().level(Level::INFO)),
-                ),
+                )
+                .layer(PropagateRequestIdLayer::new(X_REQUEST_ID.clone()))
+                .layer(SetRequestIdLayer::new(
+                    X_REQUEST_ID.clone(),
+                    MakeRequestUuid,
+                ))
+                .layer(cors),
         )
 }
 
