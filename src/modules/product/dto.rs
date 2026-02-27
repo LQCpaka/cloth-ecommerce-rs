4116bc6b16d475908b1a16e8eb8f7281ba9e9b58
LQCpaka <boyrovip1539@gmail.com>
2026-02-27 22:21:08 +0700

    product dto

--- src/modules/product/dto.rs
+++ src/modules/product/dto.rs
@@ -1,1 +1,17 @@
+use bigdecimal::BigDecimal;
+use serde::Deserialize;
+use validator::Validate;
 
+#[derive(Debug, Deserialize, Validate)]
+pub struct CreateProductRequest {
+    pub product_id: i32,
+
+    #[validate(length(min = 3, message = "Tên sản phẩm phải có ít nhất 3 ký tự"))]
+    pub name: String,
+
+    pub slug: String,
+
+    pub description: Option<String>,
+
+    pub base_price: BigDecimal,
+}
