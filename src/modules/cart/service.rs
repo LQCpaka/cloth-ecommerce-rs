26f24bc97c9a9aad18b3abf2e7e430e30e44167c
LQCpaka <boyrovip1539@gmail.com>
2026-03-08 20:58:36 +0700

    cart service

--- src/modules/cart/service.rs
+++ src/modules/cart/service.rs
@@ -1,6 +1,8 @@
 use std::sync::Arc;
 
-use crate::shared::services::redis_service::RedisService;
+use uuid::Uuid;
+
+use crate::{error::AppError, shared::services::redis_service::RedisService};
 
 pub struct CartService {
     redis_service: Arc<RedisService>,
@@ -9,5 +11,23 @@ pub struct CartService {
 impl CartService {
     pub fn new(redis_service: Arc<RedisService>) -> Self {
         Self { redis_service }
+    }
+
+    pub async fn add_item_to_card(
+        &self,
+        user_id: Uuid,
+        variant_id: Uuid,
+        quantity: i32,
+    ) -> Result<(), AppError> {
+        // Name cart for user
+        let cart_key = format!("cart;{}", user_id);
+
+        let field = variant_id.to_string();
+
+        self.redis_service
+            .hincr(&cart_key, &field, quantity)
+            .await?;
+
+        Ok(())
     }
 }
