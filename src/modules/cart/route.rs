13b42933503d6dcdb8575892e7c6555489013c08
LQCpaka <boyrovip1539@gmail.com>
2026-03-12 22:28:21 +0700

    remove model cart

--- src/modules/cart/route.rs
+++ src/modules/cart/route.rs
@@ -9,7 +9,7 @@ pub fn cart_router() -> Router<AppState> {
     Router::new()
         .route("/", post(handler::add_to_cart).get(handler::get_cart))
         .route(
-            "/:variant_id",
+            "/{variant_id}",
             put(handler::update_cart_item).delete(handler::remove_from_cart),
         )
 }
